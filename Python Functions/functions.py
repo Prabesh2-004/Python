68d362e5e253c58e6dce805f77bc13c2ade847e5
Prabesh2-004 <coder.bro2004@gmail.com>
2026-01-22 21:30:55 +0545

    function

---
+++ Python Functions/functions.py
@@ -0,0 +1,13 @@
+variable = 'Hello World!'
+def my_function():
+    print(variable)
+    
+my_function()
+
+def sum(num1, num2):
+    print(num1 + num2)
+sum(5, 7)
+
+# function cannot be empty so we have to use pass to skip function
+def  pass_fun():
+    pass
